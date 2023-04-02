9a8ca399412963e4ccdc09b805ad20ce4ed3bc7e
Mario Christofell <106422023+mariotbg11@users.noreply.github.com>
2023-04-02 10:15:35 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/* ACCORDION */
 const accordion = document.getElementsByClassName("accordion");
 
 for (let i = 0; i < accordion.length; i++) {
@@ -11,3 +12,22 @@ for (let i = 0; i < accordion.length; i++) {
     }
   });
 }
+
+/* SCROLL ANIMATION */
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 200;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
